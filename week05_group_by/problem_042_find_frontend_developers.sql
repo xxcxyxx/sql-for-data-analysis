02fd3c391a7d8e42358d4ad9024252d37b662b96
chaeyounq <71420116+xxcxyxx@users.noreply.github.com>
2026-03-05 13:55:46 +0900

    week05 - feat: Retrieve developers with frontend skills using bitwise masking

--- week05_group_by/problem_042_find_frontend_developers.sql
+++ week05_group_by/problem_042_find_frontend_developers.sql
@@ -1,16 +1,17 @@
 -- 문제: FrontEnd 개발자 찾기
--- 유형: JOIN / BITWISE 연산
--- 핵심: 개발자의 SKILL_CODE와 Front End 스킬 CODE를 비트 연산으로 비교
--- 조건: SKILL_CODE & CODE > 0
+-- 유형: BITWISE 연산 / SUBQUERY
+-- 핵심: Front End 스킬 CODE들을 합친 비트마스크와 개발자의 SKILL_CODE를 비교
+-- 조건: SKILL_CODE & (Front End CODE 합) > 0
 -- 정렬: ID ASC
 
 SELECT
-    D.ID,
-    D.EMAIL,
-    D.FIRST_NAME,
-    D.LAST_NAME
-FROM DEVELOPERS D
-JOIN SKILLCODES S
-    ON S.CATEGORY = 'Front End'
-WHERE D.SKILL_CODE & S.CODE > 0
-ORDER BY D.ID ASC;
+    ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+FROM DEVELOPERS
+WHERE (SKILL_CODE & 
+      (SELECT SUM(CODE)
+       FROM SKILLCODES
+       WHERE CATEGORY LIKE 'Front%')) > 0
+ORDER BY ID;
